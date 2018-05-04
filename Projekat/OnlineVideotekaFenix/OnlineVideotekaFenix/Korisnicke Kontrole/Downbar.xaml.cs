e813c240b80d93f01cfd43f0465df50a6df9dcd5
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2018-05-04 18:10:51 +0200

    User kontrola

---
+++ Projekat/OnlineVideotekaFenix/OnlineVideotekaFenix/Korisnicke Kontrole/Downbar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OnlineVideotekaFenix.Korisnicke_Kontrole
+{
+    public sealed partial class Downbar : UserControl
+    {
+        public DodajPoster()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonIzlaz(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+            
+        private void ButtonRegistracija(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
