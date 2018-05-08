4a6cd06fb57774214c2f7e0ca777cbf5cc775ea1
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2018-05-08 15:19:41 +0200

    Navigacija validacija (dijelom)

--- Projekat/OnlineVideotekaFenix/OnlineVideotekaFenix/Views/Login.xaml.cs
+++ Projekat/OnlineVideotekaFenix/OnlineVideotekaFenix/Views/Login.xaml.cs
@@ -22,10 +22,72 @@ namespace OnlineVideotekaFenix.Views
     /// </summary>
     public sealed partial class Login : Page
     {
+        static readonly string[] validateProperties = { "Username","Lozinka" };
         public Login()
         {
             this.InitializeComponent();
         }
-        
+
+        private void LoginClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RegistracijaClick(Object sender,RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Registracija));
+        }
+
+        private void IzlazClick(Object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+        public bool IsValid
+        {
+            get
+            {
+                foreach (string property in validateProperties)
+                {
+                    if (getValidationError(property) != null)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        string getValidationError(string propertyName)
+        {
+            string error = null;
+            switch (propertyName)
+            {
+                case "Username":
+                    error = validirajUsername();
+                    break;
+                case "Lozinka":
+                    error = validirajLozinku();
+                    break;               
+            }
+            return error;
+        }
+
+        private string validirajUsername()
+        {
+            string username = "";/* ((TextBox)Cumez.FindName("Username")).get*/
+            if (String.IsNullOrWhiteSpace(username)) return "Morate unijeti korisnicko ime!";
+            return null;
+        }        private string validirajLozinku()
+        {
+            string lozinka = "";/*                                            */
+            if(String.IsNullOrWhiteSpace(lozinka) return "Morate unijeti lozinku!";
+            return null;
+        }
+
+
+
+
+
+
     }
 }
