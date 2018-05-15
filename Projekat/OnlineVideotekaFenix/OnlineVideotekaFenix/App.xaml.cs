e3b880f6ee40c8823c52dc572b045d5d6c07e2be
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2018-05-15 03:47:46 +0200

    Jos azure

--- Projekat/OnlineVideotekaFenix/OnlineVideotekaFenix/App.xaml.cs
+++ Projekat/OnlineVideotekaFenix/OnlineVideotekaFenix/App.xaml.cs
@@ -28,7 +28,7 @@ namespace OnlineVideotekaFenix
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
-        public static MobileServiceClient MobileService = new MobileServiceClient("https://OnlineVideotekaFenix.azurewebsites.net");
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://OnlineVideoteka.azurewebsites.net");
 
         public App()
         {
