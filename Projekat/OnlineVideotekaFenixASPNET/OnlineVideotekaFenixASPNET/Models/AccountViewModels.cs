44437f1eb22734efe52a8011e38bee33417301d2
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2018-05-24 12:43:31 +0200

    Svaki pocetak je tezak

--- Projekat/OnlineVideotekaFenixASPNET/OnlineVideotekaFenixASPNET/Models/AccountViewModels.cs
+++ Projekat/OnlineVideotekaFenixASPNET/OnlineVideotekaFenixASPNET/Models/AccountViewModels.cs
@@ -49,16 +49,16 @@ namespace OnlineVideotekaFenixASPNET.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
+        [Display(Name = "Email adresa")]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lozinka")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Ostanite prijavljeni?")]
         public bool RememberMe { get; set; }
     }
 
@@ -66,17 +66,17 @@ namespace OnlineVideotekaFenixASPNET.Models
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Email adresa")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lozinka")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Potvrdite lozinku")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
